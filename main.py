dcac799a2d669d8da280115deb34065b4f48deac
Vrublevsky Aleksey <py6jlb@gmail.com>
2016-02-25 23:27:51 +0300

    try create async_download

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import crawl_parser
 from repo import SitesRepo, PageRepo, WordpairsRepo, Repo
 import time
 import os
+import async_download
 
 page_parser = crawl_parser.HtmlParser()
 url_handler = url_graber.SitemapGruber()
@@ -14,15 +15,28 @@ wordpairs = WordpairsRepo()
 
 base_repo = Repo()
 
+
 def main():
     os.chdir(r'/tmp')
+    url_tuple_list = pages.get_not_scan_urls()
+    url_list = [ url.url for url in url_tuple_list]
+    async_download.async_download(url_list)
+
+if __name__ == "__main__":
+    main()
+
+
+'''
     while True:
         site_list = sites.get_all()
         for site in site_list:
             url_list = url_handler.get_url_from_sitemap(site.name)
             pages.add_url(url_list, site.id)
-        url_tuple_list = pages.get_not_scan_urls()
-        for url_tuple in url_tuple_list:
+
+
+
+
+for url_tuple in url_tuple_list:
             query_tuple_list = wordpairs.get_query()
             file = download.download_file(url_tuple.url)
             print(url_tuple)
@@ -35,6 +49,4 @@ def main():
 
         time.sleep(43200)
 
-
-if __name__ == "__main__":
-    main()
+'''
